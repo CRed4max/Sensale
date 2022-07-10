17ac1a61d51025f2df3a29d540a56aa4c90e1cc5
rathoreDeelip <2019ucp1335@mnit.ac.in>
2022-07-11 03:07:50 +0530

    make it better

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,13 @@
-const mongoose = require("mongoose")
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_URI,).then((data)=>{
-        console.log(`Mongodb connected with server :${data.connection.host}`)
+const mongoose = require('mongoose');
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     })
-}
-module.exports =connectDatabase
+    .then((data) => {
+      console.log(`Mongodb connected with server :${data.connection.host}`);
+    });
+};
+module.exports = connectDatabase;
