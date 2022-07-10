9f92f150cd67f132944e9c296550c6f6dc48f1e9
rathoreDeelip <2019ucp1335@mnit.ac.in>
2022-07-11 02:56:18 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,41 @@
+// const app = require('./app');
+// const connectDatabase = require('./config/database');
+// const cloudinary = require('cloudinary');
+// const
+// //Handing uncaught Exception
+
+// process.on('uncaughtException', (err) => {
+//   console.log(`Error:${err.message}`);
+//   console.log(`Shutting down the server due to uncaughtException`);
+//   process.exit(1);
+// });
+
+// if (process.env.NODE_ENV !== 'PRODUCTION') {
+//   require('dotenv').config({ path: './backend/config/config.env' });
+// }
+
+// connectDatabase();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(`server is working on http://localhost:${process.env.PORT}`);
+// });
+
+// //unhandeled promise rejectionafss
+
+// process.on('unhandledRejection', (err) => {
+//   console.log(`Error:${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
 const app = require('./app');
 const connectDatabase = require('./config/database');
 const cloudinary = require('cloudinary');
