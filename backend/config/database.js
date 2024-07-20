70d346026f0c9085def9f369641930c99b37de8e
cred4max <chhotelalk2509@gmail.com>
2024-07-21 01:53:38 +0530

    code fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const connectDatabase = () => {
-  mongoose
+const connectDatabase = async () => {
+  const connect = await mongoose
     .connect(
       "mongodb+srv://mongo_chhotelal:lCiPWAaeNqOnmWJO@sensale.9sbudrz.mongodb.net/sensale_db"
     )
-    .then((data) => {
-      console.log(`Mongodb connected with server :${data.connection.host}`);
-    });
+    console.log(`Mongodb connected with server :${connect.connection.host}`);
+    return connect;
 };
 module.exports = connectDatabase;
