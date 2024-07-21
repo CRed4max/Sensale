531ffbcbe7ab007262eecf01a89e04595683bf07
cred4max <chhotelalk2509@gmail.com>
2024-07-21 10:40:38 +0530

    baseURL change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://sensale.onrender.com',
   withCredentials: true,
 });
+
+// baseURL: 'http://localhost:4000'
 
 instance.defaults.timeout = 2500;
 
